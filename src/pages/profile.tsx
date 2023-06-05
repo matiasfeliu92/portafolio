b692197d4d4eefcc9f4beb202ae9311074cfcea8
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-05 18:43:17 -0300

    refactor: update index.tsx main page and add contact, profile, skills and works pages

---
+++ src/pages/profile.tsx
@@ -0,0 +1,15 @@
+import { NavBar } from '@/components/NavBar'
+import Navbar1 from '@/components/NavBar1'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+      <Navbar1/>
+      {/* <NavBar/> */}
+      <h1>PROFILE PAGE</h1>
+    </div>
+  )
+}
+
+export default page
