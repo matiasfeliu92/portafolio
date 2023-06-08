2cdb7dab3767603f964ed7e7c1ca45b00346e1be
matiasfeliu92 <matumazparrote@gmail.com>
2023-06-08 16:37:00 -0300

    refactor: update works and skills pages, delete Skills and Works imports

--- src/pages/skills.tsx
+++ src/pages/skills.tsx
@@ -1,39 +1,53 @@
-import Navbar1 from '@/components/NavBar1'
-import { Skills } from '@/components/Skills'
-import ISkills from '@/interfaces/Skills'
-import styles from '../styles/Skills.module.css'
-import React from 'react'
+import Navbar1 from "@/components/NavBar1";
+import ISkills from "@/interfaces/Skills";
+import styles from "../styles/Skills.module.css";
+import React from "react";
 
 type Props = {
-  data: ISkills[]
-}
+  data: ISkills[];
+};
 
-const page = ({ data }:Props) => {
+const page = ({ data }: Props) => {
   return (
     <div>
-      <Navbar1/>
+      <Navbar1 />
       <section className={`${styles.compet}`}>
         <div className={`${styles.compet__hab}`}>
-          <Skills data={data} />
+          {data.map((skill: ISkills) => (
+            <div
+              className="compet__habText col-md-2 text-center flex-column justify-content-between"
+              key={skill.name}
+            >
+              <img
+                width={"85vw"}
+                height={"auto"}
+                src={skill.imageUrl}
+                alt={skill.name}
+              />
+              <h4 style={{ fontSize: "20px" }} className="text-center">
+                {skill.name}
+              </h4>
+            </div>
+          ))}
         </div>
       </section>
     </div>
-  )
-}
+  );
+};
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   try {
-    const res = await fetch(`${process.env.REACT_APP_URL}/api/skills`)
-    const data = await res.json()
+    const res = await fetch(`${process.env.REACT_APP_URL}/api/skills`);
+    const data = await res.json();
     return {
-      props: { data }
-    }
+      props: { data },
+    };
   } catch (error) {
-    console.log(error)
+    console.log(error);
     return {
-      props: { data: null }
-    }
+      props: { data: null },
+    };
   }
 }
 
-export default page
+export default page;
